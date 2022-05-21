63bba117c4f900cd393b57775573c123f25ad9f4
Daniel Perdomo <daniel.perdomo@zinobe.com>
2022-05-21 18:34:45 -0500

    added install_requires

--- setup.py
+++ setup.py
@@ -2,9 +2,29 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
-    packages=find_packages(),
+    install_requires=[
+        "pandas", 
+        "ipykernel", 
+        "numpy",
+        "sodapy",
+        "python-dotenv>=0.5.1"
+    ],
     version='0.1.0',
     description='classification of public procurement through UNSPSC codes',
-    author='Correlation One - Team 95',
+    author="""
+    Laura Navarro
+
+    Julian Vargas
+
+    Daniel Perdomo
+
+    Julian Chacon
+
+    Victor Rivera
+
+    Jose Dager
+
+    Jose Cardenas
+    """,
     license='',
 )
