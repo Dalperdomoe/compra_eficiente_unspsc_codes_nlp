257e62b3574e78ffd65923ed3a763e7783841b70
Daniel Perdomo <daniel.perdomo@zinobe.com>
2022-05-31 23:24:33 -0500

    Created function to extract raw data from SECOP II

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,64 @@
 # -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+SOCRATA_DOMAIN = 'www.datos.gov.co'
+SOCRATA_DATASET_IDENTIFIER = 'jbjy-vk9h'
+SOCRATA_TOKEN = os.environ.get("SODAPY_APPTOKEN")
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+RAW_DATA_DIR = os.path.join(
+    CURRENT_DIR,
+    os.pardir,
+    os.pardir,
+    "data",
+    "raw"
+)
+RAW_DATA_FILE = os.path.join(
+    RAW_DATA_DIR,
+    'raw_data.csv'
+)
+
+
+def secop2_data_generator():
+    """ Create SECOP II raw data using the sodapy package.
     """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+    import pandas as pd
+    from sodapy import Socrata
+
+    client = Socrata(SOCRATA_DOMAIN, SOCRATA_TOKEN)
+
+    df = pd.DataFrame(
+        client.get(
+            SOCRATA_DATASET_IDENTIFIER,
+            query="""
+                SELECT descripcion_del_proceso,
+                    codigo_de_categoria_principal
+                WHERE date_extract_y(ultima_actualizacion) IN (
+                                                            2017,
+                                                            2018,
+                                                            2019,
+                                                            2021,
+                                                            2022)
+                AND date_extract_m(ultima_actualizacion) IN (3,6,9,12)
+                LIMIT 500000
+            """
+        )
+    )
+
+    return df.to_csv(
+        RAW_DATA_FILE,
+        index=False
+    )
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    if not os.path.exists(RAW_DATA_FILE):
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+        secop2_data_generator()
 
-    main()
+    else:
+
+        print("dataset already exists.")
